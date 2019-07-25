2d2e81862d48bc71aa6eb4ed5ddc7f3c08c29eee
jaehyune <poa8810@naver.com>
2019-07-25 16:14:26 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\downloads\hackaton_hangout-master\hackaton_hangout-master\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
